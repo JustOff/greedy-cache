afc7219094a5bde139623780edbef271a43b7381
JustOff <Off.Just.Off@gmail.com>
2021-02-22 22:36:27 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.2.2
+set VER=1.2.3
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/greedy-cache-.+?\.xpi/download\/%VER%\/greedy-cache-%VER%\.xpi/" update.xml
